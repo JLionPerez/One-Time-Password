43729cd3c814ec717e19da9c657c0bb6d9cd65da
Joelle Perez <perezjoe@oregonstate.edu>
2019-11-27 21:48:47 -0800

    starting keygen

--- keygen.c
+++ keygen.c
@@ -1,1 +1,14 @@
-hello
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+
+    srand(time(NULL));
+
+    int key_length;
+    key_length = argv[1];
+    
+    char key_str[key_length];
+}
