2d87a4a9ccc6a524ca27033400d2c171a6246e8c
Joelle Perez <perezjoe@oregonstate.edu>
2019-11-27 23:04:13 -0800

    finished keygen

--- keygen.c
+++ keygen.c
@@ -9,19 +9,21 @@ int main(int argc, char *argv[]) {
 
     int key_length = 0;
     char key_str[key_length];
+    char hold;
 
-    key_length = atoi(argv[1]);
-
-    //strcpy(key_str, "hello");
+    key_length = atoi(argv[1]); //get length
 
     int i;
-    char hold;
-    for(i = 0; i < key_length; i++) {
-        hold = 'A' + (rand() % 27);
-        printf("%c", hold);
+    for(i = 0; i < key_length; i++) { //get string
+        hold = 'A' + (rand() % 27); //generate random letters
+
+        if(hold == '[') { //if not a letter turn to space
+            hold = ' ';
+        }
+
+        key_str[i] = hold; //append to string
     }
+    key_str[key_length] = '\0'; //null terminator at the end of string
 
-    //printf("%s\n", key_str);
-
-    //printf("%d\n", key_length);
+    printf("%s\n", key_str);
 }
