b85f0d1d1b1881610239f76418ca34c870400c67
Joelle Perez <perezjoe@oregonstate.edu>
2019-11-27 22:28:57 -0800

    transfer to os1

--- keygen.c
+++ keygen.c
@@ -7,8 +7,21 @@ int main(int argc, char *argv[]) {
 
     srand(time(NULL));
 
-    int key_length;
-    key_length = argv[1];
-    
+    int key_length = 0;
     char key_str[key_length];
+
+    key_length = atoi(argv[1]);
+
+    //strcpy(key_str, "hello");
+
+    int i;
+    char hold;
+    for(i = 0; i < key_length; i++) {
+        hold = 'A' + (rand() % 27);
+        printf("%c", hold);
+    }
+
+    //printf("%s\n", key_str);
+
+    //printf("%d\n", key_length);
 }
