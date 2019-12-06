e92cc30f7add7c6b34d52cd41770b2b2f766fd8d
Joelle Perez <perezjoe@oregonstate.edu>
2019-12-05 20:55:10 -0800

    no seg

--- otp_enc_d2.c
+++ otp_enc_d2.c
@@ -79,6 +79,7 @@ int main(int argc, char *argv[])
 		//charsRead = 0;
 		// charsRead = send(establishedConnectionFD, "I am the server, and I got your message", 39, 0); // Send success back
 		// if (charsRead < 0) error("ERROR writing to socket");
+		break;
 	}
 
 	close(establishedConnectionFD); // Close the existing socket which is connected to the client
